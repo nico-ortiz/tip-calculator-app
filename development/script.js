feaf91ffc91f7fc2fe78eedfeaa101802adafb7b
Nicolas-Ortiz <nicortiz1234@gmail.com>
2024-02-10 16:09:44 -0300

    Modify some HTML tags and add listener to custom percent button

--- development/script.js
+++ development/script.js
@@ -1,35 +1,71 @@
+/* Set elements from HTML */
 const billInput = document.getElementById('bill-value');
-const percentBtns = document.querySelectorAll('.percent-btns');
 const numberOfPeopleInput = document.getElementById('number-of-people');
+const percentBtns = document.querySelectorAll('.percent-btns');
+const percentCustom = document.getElementById('input-custom');
 
 const tipAmountTotal = document.getElementById('tip-amount-total');
 const amountPerPerson = document.getElementById('amount-p-person');
 
+/* This variables represent bill, select tip% and number of people */
 let billValue = 0.0;
-let numberOfPeopleValue = 0;
+let numberOfPeopleValue = 1;
 let percentBtnValue = 0;
+
+/* Initial setting for tip amount per person and total per person */
 tipAmountTotal.innerHTML = '$' + (0.0).toFixed(2);
 amountPerPerson.innerHTML = '$' + (0.0).toFixed(2);
 
-
+/* Event listeners */
 billInput.addEventListener('input', billHandle);
+
 numberOfPeopleInput.addEventListener('input', numberOfPeopleHandle);
+
 percentBtns.forEach((btn) => {
     btn.addEventListener('click', percentBtnsHandle);
 });
 
+percentCustom.addEventListener('input', percentCustomHandle);
+
 function billHandle() {
     billValue = parseFloat(billInput.value);
-    console.log(billValue);
+    calculateTipAmount();
 }
 
 function numberOfPeopleHandle() {
-    numberOfPeopleValue = parseFloat(numberOfPeopleInput.value);
-    console.log(numberOfPeopleValue);
+    numberOfPeopleValue = numberOfPeopleInput.value;
+    calculateTipAmount();
 }
 
 function percentBtnsHandle(event) {
-    percentBtnValue = event.target.value;
-    console.log(percentBtnValue);
+    let percent = event.target.innerHTML.replace('%', '');
+    percentBtnValue = percent / 100;
+    calculateTipAmount();
 }
 
+function percentCustomHandle() {
+    let percent = percentCustom.value;
+    if (percent < 1 || percent > 100) {
+        percentBtnValue = 1;
+        calculateTipAmount();
+    } else {
+        percentBtnValue = percent / 100;
+        calculateTipAmount();
+    }
+}
+
+/* Function to calculate tip amount per person and total per person */
+function calculateTipAmount() {
+    if (numberOfPeopleValue >= 1) {
+        let tipAmountPerPerson = (billValue * percentBtnValue) / numberOfPeopleValue; 
+        let totalPerPerson = (billValue / numberOfPeopleValue) + tipAmountPerPerson;
+
+        if (isNaN(tipAmountPerPerson) && isNaN(totalPerPerson)) {
+            tipAmountTotal.innerHTML = '$' + (0.0).toFixed(2);
+            amountPerPerson.innerHTML = '$' + (0.0).toFixed(2);
+        } else {
+            tipAmountTotal.innerHTML = '$' + (tipAmountPerPerson).toFixed(2);
+            amountPerPerson.innerHTML = '$' + (totalPerPerson).toFixed(2);
+        }
+    }
+}
